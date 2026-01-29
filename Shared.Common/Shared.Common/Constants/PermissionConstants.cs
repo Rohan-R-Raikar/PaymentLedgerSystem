d4ad497ed3ae24466ec9ed37a3e9d02d0c9fd6fe
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-01-29 11:59:19 +0530

    Add role and permission constants

---
+++ Shared.Common/Shared.Common/Constants/PermissionConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Common.Constants
+{
+    public static class PermissionConstants
+    {
+        public const string CreateTransaction = "CreateTransaction";
+        public const string ViewTransaction = "ViewTransaction";
+        public const string ViewLedger = "ViewLedger";
+        public const string ViewReports = "ViewReports";
+    }
+}
---
+++ Shared.Common/Shared.Common/Constants/RoleConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Common.Constants
+{
+    public static class RoleConstants
+    {
+        public const string Admin = "Admin";
+        public const string Auditor = "Auditor";
+        public const string FinanceUser = "FinanceUser";
+    }
+}
